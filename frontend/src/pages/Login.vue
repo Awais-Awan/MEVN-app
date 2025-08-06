9fd5096d4daac0d94feb02127bff585cc18760f8
Awais-Awan <awanawais400@gmail.com>
2025-08-06 23:13:14 +0500

    Make the UI consistent

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="fixed inset-0 flex items-center justify-center bg-gray-100">
-    <form @submit.prevent="handleSignIn" class="bg-white p-8 rounded shadow-md w-[400px]">
-      <h2 class="text-2xl font-bold mb-6 text-center text-gray-800">Sign In</h2>
+  <div class="fixed inset-0 flex flex-col items-center justify-center bg-orange-400">
+    <div class="absolute top-4 left-4 font-bold text-xl text-gray-800">PayPoint</div>
+    <form @submit.prevent="handleSignIn" class="bg-gray-800 p-8 rounded shadow-md w-[400px]">
+      <h2 class="text-2xl font-bold mb-6 text-center text-orange-400">Sign In</h2>
+      <p v-if="message" class="text-center text-red-500 mb-6">{{ message }}</p>
       <input v-model="form.email" type="email" placeholder="Email" class="input" required>
       <input v-model="form.password" type="password" placeholder="Password" class="input" required>
       <button type="submit" class="btn">Log In!</button>
-      <a class="navigation" @click="goToSignup()">Register?</a>
+      <a class="navigation text-orange-400 hover:text-orange-300 block mt-2" @click="goToSignup()">Register?</a>
     </form>
-    <p v-if="message" class="text-center text-red-500 mt-4">{{ message }}</p>
   </div>
 </template>
 
@@ -54,11 +55,11 @@ function goToSignup() {
 <style scoped>
 
 .input{
-    @apply w-full mb-4 px-4 py-2 border rounded;
+     @apply w-full mb-4 px-4 py-2 rounded outline-none border-2 focus:border-orange-400 focus:ring-1 focus:ring-orange-400;
 }
 
 .btn{
-    @apply w-full bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded;
+    @apply w-full bg-orange-400 hover:bg-orange-300 text-gray-800 font-bold py-2 px-4 rounded;
 }
 
 
--- frontend/src/pages/Signup.vue
+++ frontend/src/pages/Signup.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="fixed inset-0 flex items-center justify-center bg-gray-100">
-    <form @submit.prevent="handleSignup" class="bg-white p-8 rounded shadow-md w-[400px]">
-      <h2 class="text-2xl font-bold mb-6 text-center text-gray-800">Sign Up</h2>
+  <div class="fixed inset-0 flex flex-col items-center justify-center bg-orange-400">
+    <div class="absolute top-4 left-4 font-bold text-xl text-gray-800">PayPoint</div>
+    <form @submit.prevent="handleSignup" class="bg-gray-800 p-8 rounded shadow-md w-[400px]">
+      <h2 class="text-2xl font-bold mb-6 text-center text-orange-400">Sign Up</h2>
+      <p v-if="message" class="text-center text-red-500 mb-6">{{ message }}</p>
       <input v-model="form.name" type="text" placeholder="Name" class="input" required>
       <input v-model="form.email" type="email" placeholder="Email" class="input" required>
       <input v-model="form.password" type="password" placeholder="Password" class="input" required>
       <button type="submit" class="btn">Create Account!</button>
-      <a class="navigation" @click="goToSignin()">Sign In?</a>
+      <a class="navigation text-orange-400 hover:text-orange-300 block mt-2" @click="goToSignin()">Sign In?</a>
     </form>
-    <p v-if="message" class="text-center text-red-500 mt-4">{{ message }}</p>
   </div>
 </template>
 
@@ -60,12 +61,12 @@ function goToSignin(){
 
 <style scoped>
 
-.input{
-    @apply w-full mb-4 px-4 py-2 border rounded;
+.input {
+  @apply w-full mb-4 px-4 py-2 rounded outline-none border-2 focus:border-orange-400 focus:ring-1 focus:ring-orange-400;
 }
 
 .btn{
-    @apply w-full bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded;
+    @apply w-full bg-orange-400 hover:bg-orange-300 text-gray-800 font-bold py-2 px-4 rounded;
 }
 
 .navigation{
