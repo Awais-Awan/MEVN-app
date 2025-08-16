5997a337bf0573c7bf67f39138a60ef10b9dbf80
Awais-Awan <awanawais400@gmail.com>
2025-08-17 00:50:42 +0500

    feat/retrict urls after login

--- frontend/src/pages/home.vue
+++ frontend/src/pages/home.vue
@@ -1,3 +1,26 @@
 <template>
     <div>Hello G Sunao!</div>
+    <button class="btn" @click="Logout()">Logout</button>
 </template>
+
+
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+function Logout(){
+    localStorage.removeItem("token");
+    router.push("/")
+    
+}
+
+</script>
+
+
+<style scoped>
+
+.btn{
+    @apply w-full bg-orange-400 hover:bg-orange-300 text-gray-800 font-bold py-2 px-4 rounded;
+}
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,10 +5,10 @@ import welcome from '../pages/welcome.vue'
 import home from '../pages/home.vue'
 
 const routes = [
-  { path: '/', component: welcome },
-  { path: '/signup', component: Signup },
-  { path: '/signin', component: Signin },
-  {path: '/home', component: home},
+  { path: '/', component: welcome, meta:{guestOnly : true} },
+  { path: '/signup', component: Signup, meta: {guestOnly : true} },
+  { path: '/signin', component: Signin, meta: {guestOnly : true} },
+  {path: '/home', component: home, meta: {requiresAuth : true}},
 ]
 
 
@@ -16,5 +16,20 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+
+router.beforeEach((to,from,next)=>{
+  const token = localStorage.getItem("token");
+
+  if (!token && to.meta.requiresAuth){
+    next("/signin");
+  }
+  else if (token && to.meta.guestOnly){
+    next("/home");
+  }
+  else{
+    next();
+  }
+});
 
 export default router
